74555a8bd0908142d46b84738e496f6420d9855d
indexzero <charlie.robbins@gmail.com>
2013-03-11 08:05:01 -0400

    [refactor minor] Be lazier when loading reactors.

--- test/reactor/reactor-test.js
+++ test/reactor/reactor-test.js
@@ -6,40 +6,33 @@
  */
 
 var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    utile = require('utile'),
     vows = require('vows'),
     godot = require('../../lib/godot');
 
 vows.describe('godot/reactor').addBatch({
   "Godot reactor": {
     "should have the correct primiatives": function () {
-      ['around',
-       'by',
-       'change',
-       'count',
-       'console',
-       'combine',
-       'email',
-       'eventWindow',
-       'expire',
-       'forward',
-       'map',
-       'mean',
-       'movingAverage',
-       'over',
-       'rate',
-       'rollup',
-       'sms',
-       'sum',
-       'tagged',
-       'taggedAny',
-       'taggedAll',
-       'throttle',
-       'timeWindow',
-       'under',
-       'where',
-       'within'].forEach(function (method) {
-         assert.isFunction(godot.reactor.Reactor.prototype[method]);
-       });
+      var reactorDir = path.join(__dirname, '..', '..', 'lib', 'godot', 'reactor'),
+          core = ['index.js', 'reactor.js'];
+
+      fs.readdirSync(reactorDir)
+        .filter(function (file) {
+          return !~core.indexOf(file);
+        })
+        .forEach(function (file) {
+          var name   = file.replace('.js', ''),
+              parts  = name.split('-'),
+              method = parts[0];
+
+          if (parts.length > 1) {
+            method += utile.capitalize(parts[1]);
+          }
+
+          assert.isFunction(godot.reactor.Reactor.prototype[method]);
+        });
     },
     "the register() method": {
       "when redefining a method": function () {
