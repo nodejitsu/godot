2334cb16a5c2ae2f48a078d639724b94a16df5ab
Jarrett Cruger <jcrugzz@gmail.com>
2014-04-09 16:59:58 -0400

    [fix] remove and replace example server

--- example/server.js
+++ example/server.js
@@ -1,26 +1,12 @@
-/*
- * server.js :: An example server with fun reactor things happening
- *
- * (C) 2013 Nodejits Inc.
- *
- */
 
-var godot = require('../lib/godot'),
-    expire = godot.reactor().expire(5000);
+var g = require('../');
 
-//
-// This example shows a way to meausre two separate clients on the same
-// `reactor` using the power of the `by` reactor. It will create a new reactor
-// pipe-chain for each different 'service' in this case to easily assess the
-// expiration of two separate `producers`.
-//
-godot.createServer({
+g.createServer({
   type: 'tcp',
   reactors: [
-    godot.reactor()
-      .by('service',
-        expire
-        .console()
-      )
+    function (socket) {
+      socket
+        .pipe(new g.console())
+    }
   ]
 }).listen(1337);
